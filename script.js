c5374f638f80722fe7a762d78a13c670e88d74e5
xishannk <nishannk27@gmail.com>
2026-06-04 22:10:53 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,32 @@
 const canvas = document.getElementById('punkCanvas');
 const ctx = canvas.getContext('2d');
 
-window.onload = function() {
-    drawCharacter();
-};
-
 function drawCharacter() {
+    const bgSrc = document.getElementById('bgSelect').value;
+    const bodySrc = document.getElementById('bodySelect').value;
+    const eyesSrc = document.getElementById('eyesSelect').value;
+    const hairSrc = document.getElementById('hairSelect').value;
+
+    const images = [bgSrc, bodySrc, eyesSrc, hairSrc];
+    
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    const bgUrl = document.getElementById('bgSelect').value;
-    const bodyUrl = document.getElementById('bodySelect').value;
-    const eyesUrl = document.getElementById('eyesSelect').value;
-    const hairUrl = document.getElementById('hairSelect').value;
-
-    const bgImg = new Image();
-    bgImg.crossOrigin = "anonymous";
-    bgImg.src = bgUrl;
-    bgImg.onload = function() {
-        ctx.drawImage(bgImg, 0, 0, canvas.width, canvas.height);
-
-        const bodyImg = new Image();
-        bodyImg.crossOrigin = "anonymous";
-        bodyImg.src = bodyUrl;
-        bodyImg.onload = function() {
-            ctx.drawImage(bodyImg, 0, 0, canvas.width, canvas.height);
-
-            if (eyesUrl) {
-                const eyesImg = new Image();
-                eyesImg.crossOrigin = "anonymous";
-                eyesImg.src = eyesUrl;
-                eyesImg.onload = function() {
-                    ctx.drawImage(eyesImg, 0, 0, canvas.width, canvas.height);
-                    loadHair(hairUrl);
-                };
-            } else {
-                loadHair(hairUrl);
-            }
-        };
-    };
-}
-
-function loadHair(hairUrl) {
-    if (hairUrl) {
-        const hairImg = new Image();
-        hairImg.crossOrigin = "anonymous";
-        hairImg.src = hairUrl;
-        hairImg.onload = function() {
-            ctx.drawImage(hairImg, 0, 0, canvas.width, canvas.height);
-        };
-    }
+    images.forEach(src => {
+        if (src) {
+            const img = new Image();
+            img.src = src;
+            img.onload = () => {
+                ctx.drawImage(img, 0, 0, 400, 400);
+            };
+        }
+    });
 }
 
 function downloadImage() {
     const link = document.createElement('a');
-    link.download = 'my-punk-character.png';
+    link.download = 'my-punk.png';
     link.href = canvas.toDataURL();
     link.click();
 }
+
+window.onload = drawCharacter;
